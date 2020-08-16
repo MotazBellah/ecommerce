fb1ec37274965d2f2f0f014688d8e41af77f3433
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-16 15:23:47 +0200

    Created Category and Product table

--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.FloatField(default=0.0)
+    image1 = models.ImageField()
+    image2 = models.ImageField(blank=True, null=True)
+    image3 = models.ImageField(blank=True, null=True)
+    image4 = models.ImageField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.price}"
