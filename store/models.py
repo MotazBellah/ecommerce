f0e5d5899ff4e80bd2f791e0c6dd450942347e1c
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-17 13:27:15 +0200

    Update the models and save the images in images folder

--- store/models.py
+++ store/models.py
@@ -13,10 +13,10 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     price = models.FloatField(default=0.0)
-    image1 = models.ImageField()
-    image2 = models.ImageField(blank=True, null=True)
-    image3 = models.ImageField(blank=True, null=True)
-    image4 = models.ImageField(blank=True, null=True)
+    image1 = models.ImageField(upload_to='images/')
+    image2 = models.ImageField(upload_to='images/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='images/', blank=True, null=True)
+    image4 = models.ImageField(upload_to='images/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
