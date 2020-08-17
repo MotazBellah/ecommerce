4b42f62f8f5847f661930e537efef3477587ccd0
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-17 13:39:24 +0200

    Added index view that shows the category

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def index(request):
+    category = Category.objects.all()
+    context = {
+        "category": category,
+    }
+
+    return render(request, 'store/index.html', context)
