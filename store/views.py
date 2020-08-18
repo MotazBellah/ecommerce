978d568288f01d15d5fc4e3002e558751cdea586
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-18 12:33:38 +0200

    Added products view to get all the products per category

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Category, Product
 
+# Get all the category
 def index(request):
     category = Category.objects.all()
     context = {
@@ -8,3 +9,12 @@ def index(request):
     }
 
     return render(request, 'store/index.html', context)
+
+# Get all the products per category
+def products(request, category_id):
+    items = Product.objects.filter(category=category_id)
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'store/products.html', context)
