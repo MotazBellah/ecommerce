077d2f3d7601ab2c2045b7d103af9ca872ea7acf
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-29 16:14:19 +0200

    Added required attr to some input in the shipping info form

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product, Cart
+from .models import Category, Product, Cart, ShippingInfo
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Cart)
+admin.site.register(ShippingInfo)
