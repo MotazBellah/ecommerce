14582676a2d2db6dc265f5cbb17e734931b27e5f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-08-29 16:11:19 +0200

    Created Shipping info model

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Category(models.Model):
@@ -34,3 +35,13 @@ class Cart(models.Model):
     def get_total(self):
         total = self.product.price * self.quantity
         return total
+
+
+class ShippingInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address1 = models.TextField()
+    address1 = models.TextField(blank=True, null=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex], max_length=17)
+    city = models.CharField(max_length=20)
+    zip = models.IntegerField()
