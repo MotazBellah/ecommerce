cad99bbbfccaf3681682c5e7ee0cd3736836c1e4
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-11 16:55:43 +0200

    Alter the shippingInfo table and add country column

--- store/models.py
+++ store/models.py
@@ -82,9 +82,9 @@ class ShippingInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address1 = models.TextField()
     address2 = models.TextField(blank=True, null=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=17)
-    city = models.CharField(max_length=20)
+    phone = models.CharField(max_length=17)
+    country = models.CharField(max_length=20)
+    city = models.CharField(max_length=64)
     zip = models.IntegerField()
 
     def serialize(self):
