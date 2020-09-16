e90c0d5bc6badf8550cb701534bde52ed2dede8c
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-16 22:57:53 +0200

    Use AWS s3 service to store the media files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
+    'storages',
 ]
 
 SITE_ID = 1
@@ -164,8 +165,24 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "store/static"),
 )
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'store/static/images')
+AWS_ACCESS_KEY_ID = 'AKIA2H2FXQ2YIYHN3MR2'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'estore-static'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'ecommerce.storage_backends.MediaStorage'
+
+
+# MEDIA_URL = '/images/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'store/static/images')
 
 # Braintree stettings
 BT_ENVIRONMENT = 'sandbox'
